016200047e6d8c3f090b30b3695e1f77e44486f0
Dawid <dawidbury1998@gmail.com>
2019-12-15 16:03:33 +0100

    installed phpunit to project

---
+++ tests/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testAddition()
+    {
+        $this->assertEquals(5, 2+3, 'Five was expected to equal 2+3');
+    }
+}
